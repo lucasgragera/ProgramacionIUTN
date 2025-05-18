35a8a1bb3a87aaf8aa374d8f81fc83f98477796e
lucasgragera <lucasgragera51@gmail.com>
2025-05-18 16:38:29 -0300

    Recursividad Updated

--- 05.2 Recursividad/test.py
+++
@@ -1,9 +0,0 @@
-def contar_digito(numero, digito):
-    if numero == 0:
-        return 0
-    else:
-        ultimo = numero % 10
-        coincidencia = 1 if ultimo == digito else 0
-        return coincidencia + contar_digito(numero // 10, digito)
-
-print(contar_digito(12233421, 2))
